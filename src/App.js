0e5fc4d44989c5803d064377a570674450078aa7
Mostakimul Kabir <kabirsiam1998@gmail.com>
2022-05-19 01:53:38 +0600

    react router set up, shop page added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-
-import HomePage from "./pages/homepage/homepage.component.jsx";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
