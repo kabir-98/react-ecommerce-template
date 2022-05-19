f004182e57f8941377b9f381f9723dfea1079271
Mostakimul Kabir <kabirsiam1998@gmail.com>
2022-05-19 20:10:06 +0600

    react-router upgraded to V6, routing issue solved, header component added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/header/header.component";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-        </Switch>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
+        </Routes>
       </Router>
     </div>
   );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { withRouter } from "react-router-dom";
-
 import "./menu-item.styles.scss";
-
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
     className={`${size} menu-item`}
-    onClick={() => history.push(`${match.url}${linkUrl}`)}
+    onClick={() => history.push(` ${match.url} ${linkUrl} `)}
   >
     <div
       className="background-image"
@@ -20,5 +17,4 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     </div>
   </div>
 );
-
-export default withRouter(MenuItem);
+export default MenuItem;
